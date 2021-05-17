01cc323932aa3e6eb481b107fb9a7c318f931bff
Amira Kurtagić <akurtagic1@etf.unsa.ba>
2021-05-17 20:29:27 +0200

    osnovna validacija

--- src/ba.unsa.etf.nrs.projekat/KorisnikController.java
+++ src/ba.unsa.etf.nrs.projekat/KorisnikController.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.nrs.projekat;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -10,6 +11,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.time.LocalDate;
+import java.util.Locale;
 
 public class KorisnikController {
     public ImageView imgView;
@@ -34,6 +38,9 @@ public class KorisnikController {
         cmbTipKorisnika.setItems(tipoviKorisnika);
        // cmbTipKorisnika.getSelectionModel().selectFirst();
         dao = TehnickiPregledDAO.getInstance();
+
+        dateDatumPolaganjaIspita.setValue(LocalDate.now());
+        dateDatumVazenjaLicence.setValue(LocalDate.now());
 
         setDisableAllField();
 
@@ -73,10 +80,93 @@ public class KorisnikController {
         dateDatumPolaganjaIspita.setDisable(true);
         dateDatumVazenjaLicence.setDisable(true);
     }
+
+    private boolean validacija(){
+
+        if(cmbTipKorisnika.getSelectionModel().getSelectedItem().equals("Tehničar") && (fldIme.getText().equals("") || fldPrezime.getText().equals("") || fldImeOca.equals("") || fldJMBG.getText().equals("") || fldMjestoPolaganjaIspita.getText().equals("") || fldBrojLicence.getText().equals("") ||
+                dateDatumVazenjaLicence.getValue().isAfter(LocalDate.now()) || dateDatumPolaganjaIspita.getValue().isAfter(LocalDate.now()))){
+            validacijaNonTehnicar();
+            validacijaTehnicar();
+            return false;
+        }
+        else{
+            if(fldIme.getText().equals("") || fldPrezime.getText().equals("") || fldImeOca.equals("") || fldJMBG.getText().equals("")) {
+                validacijaNonTehnicar();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void validacijaTehnicar(){
+        if(fldBrojLicence.getText().equals("")){
+            fldBrojLicence.getStyleClass().remove("ispravno");
+            fldBrojLicence.getStyleClass().add("neispravno");
+        } else {
+            fldBrojLicence.getStyleClass().remove("neispravno");
+            fldBrojLicence.getStyleClass().add("ispravno");
+        }
+        if(fldMjestoPolaganjaIspita.getText().equals("")){
+            fldMjestoPolaganjaIspita.getStyleClass().remove("ispravno");
+            fldMjestoPolaganjaIspita.getStyleClass().add("neispravno");
+        }else{
+            fldMjestoPolaganjaIspita.getStyleClass().remove("neispravno");
+            fldMjestoPolaganjaIspita.getStyleClass().add("ispravno");
+        }
+        if(dateDatumPolaganjaIspita.getValue().isAfter(LocalDate.now().minusDays(1))){
+            dateDatumPolaganjaIspita.getStyleClass().remove("ispravno");
+            dateDatumPolaganjaIspita.getStyleClass().add("neispravno");
+        }else{
+            dateDatumPolaganjaIspita.getStyleClass().remove("neispravno");
+            dateDatumPolaganjaIspita.getStyleClass().add("ispravno");
+        }
+        if(dateDatumVazenjaLicence.getValue().isAfter(LocalDate.now().minusDays(1))){
+            dateDatumVazenjaLicence.getStyleClass().remove("ispravno");
+            dateDatumVazenjaLicence.getStyleClass().add("neispravno");
+        }else{
+            dateDatumVazenjaLicence.getStyleClass().remove("neispravno");
+            dateDatumVazenjaLicence.getStyleClass().add("ispravno");
+        }
+    }
+    private void validacijaNonTehnicar(){
+        if(fldIme.getText().equals("")){
+            fldIme.getStyleClass().remove("ispravno");
+            fldIme.getStyleClass().add("neispravno");
+
+        } else{
+            fldIme.getStyleClass().remove("neispravno");
+            fldIme.getStyleClass().add("ispravno");
+        }
+        if(fldPrezime.getText().equals("")){
+            fldPrezime.getStyleClass().remove("ispravno");
+            fldPrezime.getStyleClass().add("neispravno");
+
+        } else{
+            fldPrezime.getStyleClass().remove("neispravno");
+            fldPrezime.getStyleClass().add("ispravno");
+        }
+        if(fldImeOca.getText().equals("")){
+            fldImeOca.getStyleClass().remove("ispravno");
+            fldImeOca.getStyleClass().add("neispravno");
+
+        } else{
+            fldImeOca.getStyleClass().remove("neispravno");
+            fldImeOca.getStyleClass().add("ispravno");
+        }
+        if(fldJMBG.getText().equals("")){
+            fldJMBG.getStyleClass().remove("ispravno");
+            fldJMBG.getStyleClass().add("neispravno");
+
+        } else{
+            fldJMBG.getStyleClass().remove("neispravno");
+            fldJMBG.getStyleClass().add("ispravno");
+        }
+    }
     public void onActionOtkazi(ActionEvent actionEvent) {
         ((Stage)imgView.getScene().getWindow()).close();
     }
 
     public void onActionPotvrdi(ActionEvent actionEvent) {
+        if(validacija() == true) ((Stage)imgView.getScene().getWindow()).close();
     }
 }
